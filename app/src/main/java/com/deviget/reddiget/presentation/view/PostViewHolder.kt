eb1411c44dafe0b0c58712ca7974e9568a63e080
Roman Salvatore <rd.salvatore@gmail.com>
2020-11-11 15:16:43 -0300

    Implement basic PostsAdapter
    This is a temporary implementation to be able to test the other components. Final UI will be the last thing to be implemented.

--- app/src/main/java/com/deviget/reddiget/presentation/view/PostViewHolder.kt
+++ app/src/main/java/com/deviget/reddiget/presentation/view/PostViewHolder.kt
@@ -1,7 +1,27 @@
 package com.deviget.reddiget.presentation.view
 
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.deviget.reddiget.R
 
-class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class PostViewHolder(
+    itemView: View,
+    private val onDismiss: (position: Int) -> Unit
+) : RecyclerView.ViewHolder(itemView) {
+    val titleText: TextView = itemView.findViewById(R.id.text_title)
+    val authorText: TextView = itemView.findViewById(R.id.text_author)
+    val dateText: TextView = itemView.findViewById(R.id.text_date)
+    val commentCountText: TextView = itemView.findViewById(R.id.text_comment_count)
+    val readStatusText: TextView = itemView.findViewById(R.id.text_read_status)
+    val image: ImageView = itemView.findViewById(R.id.image)
+    val dismissButton: Button = itemView.findViewById(R.id.button_dismiss)
+
+    init {
+        dismissButton.setOnClickListener {
+            onDismiss(adapterPosition)
+        }
+    }
 }
--- app/src/main/java/com/deviget/reddiget/presentation/view/PostsAdapter.kt
+++ app/src/main/java/com/deviget/reddiget/presentation/view/PostsAdapter.kt
@@ -1,18 +1,47 @@
 package com.deviget.reddiget.presentation.view
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.deviget.reddiget.R
+import com.deviget.reddiget.data.datamodel.Post
+import java.text.SimpleDateFormat
+import java.util.*
 
-class PostsAdapter : RecyclerView.Adapter<PostViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
-        TODO("Not yet implemented")
-    }
+private fun Date.format() = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(this)
 
+class PostsAdapter(
+    private val onPostDismissed: (post: Post, position: Int) -> Unit = { _, _ -> }
+) : RecyclerView.Adapter<PostViewHolder>() {
+
+    private val posts: MutableList<Post> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder =
+        PostViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false)
+        ) { position -> onPostDismissed(posts[position], position) }
+
+    //TODO: hardcoded strings will be fixed when the final UI is implemented
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = posts[position]
+        holder.titleText.text = item.text
+        holder.authorText.text = item.author
+        holder.dateText.text = item.date.format()
+        holder.commentCountText.text = "${item.commentCount} comments"
+        holder.readStatusText.text = if (item.read) "Read" else "Unread"
+        item.imageUrl?.let { uri ->
+            Glide.with(holder.itemView).load(uri).into(holder.image)
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = posts.size
+
+    fun setPosts(posts: List<Post>) {
+        //TODO: This will go when pagination is implemented
+        this.posts.clear()
+        this.posts.addAll(posts)
+        notifyDataSetChanged()
     }
+
 }
