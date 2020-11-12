35a47f39dc934c13a9068393353961fb591efaa3
Roman Salvatore <rd.salvatore@gmail.com>
2020-11-12 20:29:05 -0300

    Hook Toolbar with NavController

--- app/src/main/java/com/deviget/reddiget/presentation/view/ReddigetActivity.kt
+++ app/src/main/java/com/deviget/reddiget/presentation/view/ReddigetActivity.kt
@@ -4,17 +4,31 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.deviget.reddiget.R
 
 class ReddigetActivity : AppCompatActivity() {
 
     private lateinit var views: Views
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reddiget)
         views = Views(this)
         setSupportActionBar(views.toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(
+            navController,
+            appBarConfiguration
+        )
+        views.toolbar.setupWithNavController(
+            navController,
+            appBarConfiguration
+        )
     }
 
     private class Views(
